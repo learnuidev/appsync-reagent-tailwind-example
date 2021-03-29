0a3b8f9b7f3d784560662b9d8b54463e36c64d6d
Vishal Gautam <vishal.gautam@sofdesk.com>
2021-03-29 14:32:26 -0400

    init

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,12 +1,13 @@
 (ns app.core
-  "This namespace contains your application and is the entrypoint for 'yarn start'."
-  (:require [reagent.core :as r]
-            [app.hello :refer [hello]]))
+  (:require [reagent.dom :as rdom]))
+
+(defn app []
+  [:div "Hello Twitter"])
 
 (defn ^:dev/after-load render
   "Render the toplevel component for this app."
   []
-  (r/render [hello] (.getElementById js/document "app")))
+  (rdom/render [app] (.getElementById js/document "app")))
 
 (defn ^:export main
   "Run application startup logic."
